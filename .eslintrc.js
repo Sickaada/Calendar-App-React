7f27178d0ccd7bac538b34ca6bef72379c1801bd
Sickaada <silentcicada3301@gmail.com>
2022-01-31 07:06:41 +0530

    Adding clientid and api key to deploy on vercel

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,17 +6,32 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
   ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
+    ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react'],
   rules: {
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'react/jsx-indent': ['error', 4],
+    indent: 0,
+    'no-tabs': 0,
+    'max-len': ['error', { code: 150 }],
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
   },
 };
---
+++ src/config.js
@@ -0,0 +1,2 @@
+export const REACT_APP_API_KEY = 'AIzaSyAoek7oNspIoqyyRTv1GQBrX7zGrTfKaWo';
+export const REACT_APP_CLIENT_ID = '65367275187-2rtntb1b7th5nm03c83a2hgio2dt0oom.apps.googleusercontent.com';
